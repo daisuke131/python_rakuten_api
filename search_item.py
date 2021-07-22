61e4df59c7fa16cbe1ae96d297d687ccdf722250
daisuke <dada.00131@gmail.com>
2021-07-22 18:17:42 +0900

    csv書き込み機能追加

--- search_item.py
+++ search_item.py
@@ -1,9 +1,16 @@
+import os
+from datetime import datetime
+
+import pandas as pd
+
 from common import execute_api
 
 RAKUTEN_ITEM_SEARCH_URL = (
     "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
 )
 APP_ID = "1019079537947262807"
+
+WRITE_CSV_PATH = os.path.join(os.getcwd(), "csv/search_{keyword}_{datetime}.csv")
 
 
 def search_items(keyword: str):
@@ -14,6 +21,27 @@ def search_items(keyword: str):
         "keyword": keyword,
     }
     result = execute_api(url, params)
+    df = pd.DataFrame()
+    for item_data in result["Items"]:
+        item_details = item_data["Item"]
+        df = df.append(
+            {
+                "itemCode": item_details["itemCode"],
+                "itemName": item_details["itemName"],
+                "itemPrice": item_details["itemPrice"],
+                "shopCode": item_details["shopCode"],
+                "shopName": item_details["shopName"],
+                "itemUrl": item_details["itemUrl"],
+            },
+            ignore_index=True,
+        )
+    df.to_csv(
+        WRITE_CSV_PATH.format(
+            keyword=keyword, datetime=datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        ),
+        index=False,
+        encoding="utf-8-sig",
+    )
     return result
 
 
