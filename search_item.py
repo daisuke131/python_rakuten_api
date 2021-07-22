ef2892486b54ed54159931d83af5977f2b0a02d9
daisuke <dada.00131@gmail.com>
2021-07-22 22:23:49 +0900

    変数修正

--- search_item.py
+++ search_item.py
@@ -13,18 +13,16 @@ RAKUTEN_ITEM_SEARCH_URL = (
     "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706"
 )
 APP_ID = "1019079537947262807"
-
 WRITE_CSV_PATH = os.path.join(os.getcwd(), "csv/search_{keyword}_{datetime}.csv")
 
 
 def search_items(keyword: str, is_csv_output: bool):
-    url = RAKUTEN_ITEM_SEARCH_URL
     params = {
         "applicationId": APP_ID,
         "format": "json",
         "keyword": keyword,
     }
-    result = execute_api(url, params)
+    result = execute_api(RAKUTEN_ITEM_SEARCH_URL, params)
     if is_csv_output:
         df = pd.DataFrame()
         for item_data in result["Items"]:
